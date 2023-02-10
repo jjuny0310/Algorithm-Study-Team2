a85f8fc146d3d2d2a56fff05206843cbcd7d0882
이용준 <jjuny7712@gmail.com>
2023-02-10 15:08:09 +0900

    docs: bj_1978 done.

---
+++ bj_1978/bj_1978_lyj.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bj_1978_lyj {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (isPrime(sc.nextInt()))
+                count++;
+        }
+        System.out.println(count);
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1)
+            return false;
+        if (n == 2)
+            return true;
+
+        for (int i = 2; i < (Math.sqrt(n) + 1); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
